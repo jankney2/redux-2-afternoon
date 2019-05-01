1c7d08ed82ff1b542835e79c644af4e93492428c
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-05-01 15:54:22 -0600

    init commit

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,21 @@
+
+
+const initialState= {
+  purchases: [], 
+  budgetLimit: null, 
+  loading: false, 
+}
+
+
+export default function budgetReducer(state= initialState, action){
+
+  switch(action.type){
+    
+
+
+
+
+    default: 
+    return state
+  }
+}
